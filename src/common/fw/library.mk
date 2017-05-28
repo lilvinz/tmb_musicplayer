d21d2a6e37602db9e7a3a44f50c021643a6fa50e
janfietz <janfietz@gmail.com>
2017-05-28 20:22:26 +0200

    Add cpp sources

--- src/common/fw/library.mk
+++ src/common/fw/library.mk
@@ -2,4 +2,11 @@
 
 COMMON_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(COMMON_DIR)/*.c)
+CPPSRC += $(wildcard $(COMMON_DIR)/*.cpp)
+
+CSRC += $(wildcard $(COMMON_DIR)/../*.c)
+CPPSRC += $(wildcard $(COMMON_DIR)/../*.cpp)
+
+
 EXTRAINCDIRS += $(COMMON_DIR)
+EXTRAINCDIRS += $(COMMON_DIR)/..
--- src/common/ut/library.mk
+++ src/common/ut/library.mk
@@ -2,6 +2,11 @@
 
 COMMON_UT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(COMMON_UT_DIR)/*.c)
+CPPSRC += $(wildcard $(COMMON_UT_DIR)/*.cpp)
+
+CSRC += $(wildcard $(COMMON_UT_DIR)/../*.c)
+CPPSRC += $(wildcard $(COMMON_UT_DIR)/../*.cpp)
+
 EXTRAINCDIRS += $(COMMON_UT_DIR)
 
 EXTRAINCDIRS += $(COMMON_UT_DIR)/..
